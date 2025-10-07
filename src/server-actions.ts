ef58112aa6305d537395a39720fb8c1953310fec
Shahram Roohani <sh.roohani@gmail.com>
2025-10-07 08:19:50 +0100

    Replace Appwrite client SDK with server SDK

--- src/server-actions.ts
+++ src/server-actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Client, ExecutionMethod, Functions } from "appwrite";
+import { Client, ExecutionMethod, Functions } from "node-appwrite";
 
 export const sendMessage = async (body: string) => {
   const client = new Client();
@@ -8,17 +8,18 @@ export const sendMessage = async (body: string) => {
 
   client
     .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT!)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!);
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
+    .setKey(process.env.APPWRITE_FUNCTION_API_KEY!);
 
   try {
-    await functions.createExecution(
-      process.env.APPWRITE_FUNCTION_ID_SEND_MESSAGE!,
+    await functions.createExecution({
+      functionId: process.env.APPWRITE_FUNCTION_ID_SEND_MESSAGE!,
       body,
-      false,
-      "/",
-      ExecutionMethod.POST,
-      { "Content-Type": "application/json" }
-    );
+      async: false,
+      xpath: "/",
+      method: ExecutionMethod.POST,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error(`Error: ${error}`);
   }
